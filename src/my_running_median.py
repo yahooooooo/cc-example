23dc3ec605b95a65915fda3b29e2ac8cf9295e7b
Abhinav Maurya <ahmaurya@cmu.edu>
2015-03-08 01:17:17 -0500

    First draft of working word count and running median programs

--- src/my_running_median.py
+++ src/my_running_median.py
@@ -1,1 +1,67 @@
-# example of running median program
+import os, fileinput, sys
+import concurrent.futures	
+
+class CountDict:
+	count_dict = {}
+	total_count = 0
+	median = -1
+
+	def InsertElement(self, x):
+		self.total_count += 1
+		try:
+			self.count_dict[x] += 1
+		except KeyError as e:
+			self.count_dict[x] = 1
+		if self.median == -1:
+			self.median = 1.0*x
+		else:
+			self.RecalculateMedian()
+
+	def RecalculateMedian(self):
+		self.median = -1		
+		unique_counts = sorted(list(self.count_dict.keys()))
+
+		cum_sum = [0.0]
+		for key in unique_counts:
+			cum_sum.append(self.count_dict[key] + cum_sum[len(cum_sum)-1])
+		cum_sum = cum_sum[1:]
+
+		for i in range(len(cum_sum)):
+			if cum_sum[i] == self.total_count/2.0:
+				self.median = (unique_counts[i] + unique_counts[i+1])/2.0
+				break
+			if cum_sum[i] > self.total_count/2.0:
+				self.median = 1.0*unique_counts[i]
+				break
+
+	def GetMedian(self):
+		return self.median
+
+def GetWordCountsByLine(filepath):
+	word_counts = []
+	for line in fileinput.input(filepath):
+		if line.strip() == '':
+			word_count = 0
+		else:
+			word_count = line.strip().count(' ') + 1
+		word_counts.append(word_count)
+	return word_counts
+
+def main():
+	input_dir = sys.argv[1]
+	output_file = sys.argv[2]
+	
+	countDict = CountDict()
+	filepaths = [os.path.join(input_dir,filename) for filename in os.listdir(input_dir)]
+	output = open(output_file, 'w+')
+
+	with concurrent.futures.ProcessPoolExecutor(max_workers=25) as executor:
+		for word_counts in executor.map(GetWordCountsByLine, filepaths):
+			for word_count in word_counts:
+				countDict.InsertElement(word_count)
+				output.write(str(countDict.GetMedian()) + '\n')
+
+	output.close()
+
+if __name__ == '__main__':
+	main()
--- src/my_word_count.py
+++ src/my_word_count.py
@@ -1,1 +1,44 @@
-# example of word_count program
+import os, fileinput, sys
+import concurrent.futures
+import functools
+
+def GetWordCountsForFile(filepath):
+	word_counts = {}
+	for line in fileinput.input(filepath):
+		line = ''.join(ch for ch in line if ch.isalnum() or ch == ' ')
+		for word in line.strip().lower().split(' '):
+			if word == '':
+				continue
+			try:
+				word_counts[word] += 1
+			except KeyError as e:
+				word_counts[word] = 1
+	return word_counts
+
+def ReduceDictionaries(x, y):
+	result = x
+	for key in y.keys():
+		try:
+			result[key] += 1
+		except KeyError as e:
+			result[key] = 1
+	return result
+
+def main():
+	input_dir = sys.argv[1]
+	output_file = sys.argv[2]
+	
+	filepaths = [os.path.join(input_dir,filename) for filename in os.listdir(input_dir)]
+	output = open(output_file, 'w+')
+
+	with concurrent.futures.ProcessPoolExecutor(max_workers=25) as executor:
+		word_counts = executor.map(GetWordCountsForFile, filepaths)
+		result = functools.reduce(ReduceDictionaries, word_counts)
+	
+	for key in sorted(list(result.keys())):
+		output.write(key + '\t' + str(result[key]) + '\n')
+
+	output.close()
+
+if __name__ == '__main__':
+	main()
